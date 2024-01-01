78ff7dc43d14d107a2f26f3899cc8360816abbc8
Zinaaan <jiaowonanzai@gmail.com>
2024-01-01 14:55:57 -0500

    Add more jvm relevant metrics

--- src/main/java/metrics/MetricsManager.java
+++ src/main/java/metrics/MetricsManager.java
@@ -2,12 +2,12 @@ package metrics;
 
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Mono;
 
 import java.util.Collection;
 
@@ -17,27 +17,23 @@ import java.util.Collection;
  * @description
  */
 public class MetricsManager {
-    private final Logger log = LoggerFactory.getLogger(MetricsManager.class);
     private final MeterRegistry memoryRegistry = new SimpleMeterRegistry();
     private final MeterRegistry gcRegistry = new SimpleMeterRegistry();
-    //    private final Map<JvmIndicator, Double> metricMap;
+    private final MeterRegistry classLoaderRegistry = new SimpleMeterRegistry();
+    private final MeterRegistry processorRegistry = new SimpleMeterRegistry();
+    private final MeterRegistry threadRegistry = new SimpleMeterRegistry();
 
     public MetricsManager() {
-        JvmMemoryMetrics memoryMetrics = new JvmMemoryMetrics();
-        JvmGcMetrics gcMetrics = new JvmGcMetrics();
-        memoryMetrics.bindTo(memoryRegistry);
-        gcMetrics.bindTo(gcRegistry);
-//        metricMap = new HashMap<>(memoryRegistry.getMeters().size());
-//        memoryRegistry.getMeters().forEach(meter -> {
-//            metricMap.put(new JvmIndicator(meter.getId().getName(), meter.getId().getTag("area"), meter.getId().getTag("id"), meter.getId().getDescription()), meter.measure().iterator().next().getValue());
-//        });
+        new ClassLoaderMetrics().bindTo(classLoaderRegistry);
 
-//        log.info("Metrics: {}", metricMap);
+        new JvmMemoryMetrics().bindTo(memoryRegistry);
+
+        new JvmGcMetrics().bindTo(gcRegistry);
+
+        new ProcessorMetrics().bindTo(processorRegistry);
+
+        new JvmThreadMetrics().bindTo(threadRegistry);
     }
-
-//    public Map<JvmIndicator, Double> getMetricMap() {
-//        return metricMap;
-//    }
 
     public MeterRegistry getMemoryRegistry() {
         return memoryRegistry;
