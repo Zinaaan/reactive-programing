8cde8ae2d409b13a12f6426f8051d28a5a08d9ce
Zinaaan <jiaowonanzai@gmail.com>
2024-02-03 23:50:26 -0500

    Added back pressure treatment of react programing

--- src/main/java/examples/Example.java
+++ src/main/java/examples/Example.java
@@ -1,13 +1,13 @@
 package examples;
 
 import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author lzn
@@ -28,26 +28,42 @@ public class Example {
         // The data won't start flowing until we subscribe
 //        Flux.just(1, 2, 3, 4).log().subscribe(elements::add);
 
-        Flux.just(1, 2, 3, 4).log().subscribe(new Subscriber<Integer>() {
-            @Override
-            public void onSubscribe(Subscription s) {
-                s.request(Long.MAX_VALUE);
-            }
+//        Flux.just(1, 2, 3, 4).log().subscribe(new Subscriber<Integer>() {
+//            @Override
+//            public void onSubscribe(Subscription s) {
+//                s.request(Long.MAX_VALUE);
+//            }
+//
+//            @Override
+//            public void onNext(Integer integer) {
+//                elements.add(integer);
+//            }
+//
+//            @Override
+//            public void onError(Throwable t) {
+//
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//
+//            }
+//        });
 
-            @Override
-            public void onNext(Integer integer) {
-                elements.add(integer);
-            }
+//        Flux.interval(Duration.ofMillis(1))
+//                .log()
+//                .concatMap(x -> Mono.delay(Duration.ofMillis(100)))
+//                .blockLast();
+        Flux.interval(Duration.ofMillis(1))
+                .onBackpressureDrop()
+                .concatMap(a -> Mono.delay(Duration.ofMillis(100)).thenReturn(a))
+                .doOnNext(a -> System.out.println("Thread: " + Thread.currentThread().getName() + ", Element kept by consumer: " + a))
+                .subscribe();
 
-            @Override
-            public void onError(Throwable t) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        });
+        try {
+            TimeUnit.SECONDS.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
